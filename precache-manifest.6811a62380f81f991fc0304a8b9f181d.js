85208ed57f80a86ae469310e039801c67e9745e9
Rich Wareham <rich@richwareham.com>
2019-03-02 22:16:57 +0000

    Updates

---
+++ precache-manifest.6811a62380f81f991fc0304a8b9f181d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a72cf08f541cb4431f5",
+    "url": "/voucher-tool/static/js/runtime~main.4a72cf08.js"
+  },
+  {
+    "revision": "864242be516fac4627b2",
+    "url": "/voucher-tool/static/js/main.864242be.chunk.js"
+  },
+  {
+    "revision": "6f9c6eed9e4dcb05ecef",
+    "url": "/voucher-tool/static/js/2.6f9c6eed.chunk.js"
+  },
+  {
+    "revision": "864242be516fac4627b2",
+    "url": "/voucher-tool/static/css/main.a28c2d1f.chunk.css"
+  },
+  {
+    "revision": "6e99d7f0cceccf8a2eecbf0fb3fefa01",
+    "url": "/voucher-tool/index.html"
+  }
+];
